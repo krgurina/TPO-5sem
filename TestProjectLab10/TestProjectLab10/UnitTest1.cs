6d79d05367ca458e1fe417b749d7582b5c39bfad
Kristina <kristina.gurina.03@gmail.com>
2023-12-08 20:30:32 +0300

    Fix lab10

---
+++ TestProjectLab10/TestProjectLab10/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestProjectLab10
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
